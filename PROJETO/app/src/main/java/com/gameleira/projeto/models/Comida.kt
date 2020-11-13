8a2c7593a48c43746ec43b998f3a1a0829f52c38
GAMELEIRA <gameleira270499@gmail.com>
2020-11-13 17:38:20 -0300

    Corrigindo inconsistências do projeto

--- PROJETO/app/src/main/java/com/gameleira/projeto/models/Comida.kt
+++
@@ -1,3 +0,0 @@
-package com.gameleira.projeto.models
-
-data class Comida (var nome: String, var descricao: String, var img: Int)
--- PROJETO/app/src/main/java/com/gameleira/projeto/models/Restaurante.kt
+++
@@ -1,3 +0,0 @@
-package com.gameleira.projeto.models
-
-data class Restaurante (val nome: String, val endereco: String, val horarioFechamento: String, val imagem: Int,val pratos: List<Comida>)
